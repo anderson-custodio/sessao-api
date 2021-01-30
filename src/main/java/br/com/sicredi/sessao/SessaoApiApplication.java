31637ff8fe36d119b44d0e65b4724c51fc1ad913
Anderson Fernando Custódio <anderson.custodio@softplan.com.br>
2021-01-30 13:08:00 -0300

    Cria projeto Spring Boot usando Initializr.

---
+++ src/main/java/br/com/sicredi/sessao/SessaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredi.sessao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessaoApiApplication.class, args);
+	}
+
+}
