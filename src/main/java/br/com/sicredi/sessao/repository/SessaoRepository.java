25f76cb9762ead326105772324f2899d8c6a694a
Anderson Fernando Custódio <afcustodioo@gmail.com>
2021-01-30 19:46:52 -0300

    Adiciona repositories para as entidades.

---
+++ src/main/java/br/com/sicredi/sessao/repository/SessaoRepository.java
@@ -0,0 +1,9 @@
+package br.com.sicredi.sessao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.sicredi.sessao.domain.Sessao;
+
+public interface SessaoRepository extends JpaRepository<Sessao, Long> {
+
+}
